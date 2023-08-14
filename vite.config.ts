03e78a115734cbb571415cb7e306ff1ecdda094a
Alexander Brown <DrJKL0424@gmail.com>
2023-08-14 15:13:16 -0700

    Try out million automatic mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
 
+import million from 'million/compiler';
+
 import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react()],
+    plugins: [million.vite({ auto: true }), react()],
     base: '/wildcard-browser/',
   };
 });
