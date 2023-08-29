4e260fb8072c4ce452116c67212cc0dc0e21c68a
Alexander Brown <DrJKL0424@gmail.com>
2023-08-29 14:20:35 -0700

    Move copying notification to snackbar

--- src/WildcardList.tsx
+++ src/WildcardList.tsx
@@ -4,6 +4,7 @@ import {
   Shuffle,
   ContentCopy,
   DataObject,
+  Close,
 } from '@mui/icons-material';
 import {
   Collapse,
@@ -12,6 +13,8 @@ import {
   ListItemText,
   IconButton,
   Tooltip,
+  Snackbar,
+  Alert,
 } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -51,7 +54,7 @@ export function WildcardList({
   const matchesSearch =
     !!search && wildcards.filepath.toLowerCase().includes(search.toLowerCase());
 
-  const [showCopied, setShowCopied] = useState(false);
+  const [showCopied, setShowCopied] = useState<string | null>(null);
   const settings = useContext(WildcardSettingsContext);
 
   const handlePlaceholderClick: MouseEventHandler = async (
@@ -65,8 +68,9 @@ export function WildcardList({
       selection?.removeAllRanges();
       selection?.addRange(range);
     }
-    await copyToClipboard(wildcards.toPlaceholder());
-    setShowCopied(true);
+    const text = wildcards.toPlaceholder(settings);
+    await copyToClipboard(text);
+    setShowCopied(`Copied ${text}`);
   };
 
   const handleDynamicPromptClick: MouseEventHandler = async (
@@ -80,95 +84,114 @@ export function WildcardList({
       selection?.removeAllRanges();
       selection?.addRange(range);
     }
-    await copyToClipboard(wildcards.toDynamicPromptSnippet());
-    setShowCopied(true);
+    const text = wildcards.toDynamicPromptSnippet();
+    await copyToClipboard(text);
+    setShowCopied(`Copied ${text}`);
   };
 
   const handleCopiedClose = () => {
-    setShowCopied(false);
+    setShowCopied(null);
   };
   const shuffleClick = () => {
     wildcards.setRandomEntry();
     forceUpdate();
   };
   return (
-    <Card variant="outlined" className="mt-1 mx-1" key={filename}>
-      <CardHeader
-        sx={{
-          '& .MuiCardHeader-content': {
-            minWidth: '0',
-          },
-          '& .MuiCardHeader-action': {
-            alignSelf: 'center',
-          },
-        }}
-        avatar={
-          <IconButton
-            aria-label=""
-            className="flex-shrink"
-            onClick={toggleListOpen}>
-            {listOpen ? <ExpandLess /> : <ExpandMore />}
-          </IconButton>
-        }
-        title={WildcardHeaderTitle({
-          wildcards,
-          matchesSearch,
-          onClick: toggleListOpen,
-          isOpen: listOpen,
-          forceUpdate,
-        })}
-        subheader={
-          <span className="truncate">
-            {wildcards.getSelectedEntry() || '\u00A0'}
-          </span>
-        }
-        action={
-          <>
-            <IconButton className="flex-auto" onClick={shuffleClick}>
-              <Shuffle />
+    <>
+      <Card variant="outlined" className="mt-1 mx-1" key={filename}>
+        <CardHeader
+          sx={{
+            '& .MuiCardHeader-content': {
+              minWidth: '0',
+            },
+            '& .MuiCardHeader-action': {
+              alignSelf: 'center',
+            },
+          }}
+          avatar={
+            <IconButton
+              aria-label=""
+              className="flex-shrink"
+              onClick={toggleListOpen}>
+              {listOpen ? <ExpandLess /> : <ExpandMore />}
             </IconButton>
-            <Tooltip
-              title={`${wildcards.toPlaceholder(settings)}`}
-              open={showCopied}
-              onClose={handleCopiedClose}
-              placement="left"
-              arrow>
-              <IconButton
-                className="flex-auto"
-                onClick={handlePlaceholderClick}>
-                <ContentCopy />
+          }
+          title={WildcardHeaderTitle({
+            wildcards,
+            matchesSearch,
+            onClick: toggleListOpen,
+            isOpen: listOpen,
+            forceUpdate,
+          })}
+          subheader={
+            <span className="truncate">
+              {wildcards.getSelectedEntry() || '\u00A0'}
+            </span>
+          }
+          action={
+            <>
+              <IconButton className="flex-auto" onClick={shuffleClick}>
+                <Shuffle />
               </IconButton>
-            </Tooltip>
-            <Tooltip
-              title={`${wildcards.toDynamicPromptSnippet()}`}
-              open={showCopied}
-              onClose={handleCopiedClose}
-              placement="left"
-              arrow>
-              <IconButton
-                className="flex-auto"
-                onClick={handleDynamicPromptClick}>
-                <DataObject />
-              </IconButton>
-            </Tooltip>
-          </>
-        }
-        className={`wildcards-filename`}
-      />
-      <Collapse in={listOpen} mountOnEnter={true}>
-        <CardContent>
-          <FixedSizeList
-            height={400}
-            width={'100%'}
-            itemSize={50}
-            itemCount={entries.length}
-            overscanCount={5}
-            itemData={{ entries, wildcards, forceUpdate }}>
-            {WildcardEntry}
-          </FixedSizeList>
-        </CardContent>
-      </Collapse>
-    </Card>
+              <Tooltip
+                enterDelay={1000}
+                title={`Copy as Wildcard`}
+                placement="left"
+                arrow>
+                <IconButton
+                  className="flex-auto"
+                  onClick={handlePlaceholderClick}>
+                  <ContentCopy />
+                </IconButton>
+              </Tooltip>
+              <Tooltip
+                enterDelay={1000}
+                title={`Copy as Dynamic Prompt`}
+                placement="left"
+                arrow>
+                <IconButton
+                  className="flex-auto"
+                  onClick={handleDynamicPromptClick}>
+                  <DataObject />
+                </IconButton>
+              </Tooltip>
+            </>
+          }
+          className={`wildcards-filename`}
+        />
+        <Collapse in={listOpen} mountOnEnter={true}>
+          <CardContent>
+            <FixedSizeList
+              height={400}
+              width={'100%'}
+              itemSize={50}
+              itemCount={entries.length}
+              overscanCount={5}
+              itemData={{ entries, wildcards, forceUpdate }}>
+              {WildcardEntry}
+            </FixedSizeList>
+          </CardContent>
+        </Collapse>
+      </Card>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        open={showCopied !== null}
+        onClose={handleCopiedClose}>
+        <Alert
+          severity="info"
+          action={
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleCopiedClose}>
+              <Close fontSize="small" />
+            </IconButton>
+          }>
+          {showCopied}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
 
